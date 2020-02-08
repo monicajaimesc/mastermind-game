84abc08a4667598ef05a123a94da745eed6ed543
Fundacion CODERISE <fundacioncoderise@iMac-de-Fundacion.local>
2020-02-07 19:21:11 -0500

    update all the interfase

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import math
 #palette of colors
 RED = (255,0,0)
 BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
+GREEN = (0, 255, 0, 0.5)
 YELLOW = (255,255,0)
 GRAY = (158, 148, 146)
 BLACK = (0, 245, 0)
 WHITE = (255, 255, 255)
+TRANSPARENCY = (0, 0, 0, 125)
 
 def main():
     global run, screen, circleRed, circleGreen, circleYellow, circleBlue
@@ -24,20 +25,52 @@ def main():
     icon = py.image.load('brain.png')
     py.display.set_icon(icon)
     # create a surface into screen
-    rectangle = py.Surface((0.4 * 800, 0.7 * 600 + 50))
+    rectangle = py.Surface((0.4 * 800, 0.8 * 600 + 50))
     rectangle.fill(GRAY)
     screen.blit(rectangle, (110, 100))
     #py.draw.rect(screen, GRAY, (110, 100, 0.7 * 800, 0.7 * 600 + 50))
+    py.draw.rect(screen, GRAY, (110, 100, 0.7 * 800, 0.7 * 600 + 50))
+    # Botton save
+    py.draw.circle(screen, RED, (int(0.55 * 800), int(0.90 * 600)), 20)
+    positions = []
     for i in range(4):
+        layer = []
         for j in range(9):
+            layer.append((175 + 60* i, 130 + 50*j))
             py.draw.circle(screen, WHITE, (175 + 60* i, 130 + 50*j), 20)
+        positions.append(layer)
 
+    # traspone postion into resutl 
+    result = []
+    for i in range(len(positions[0])):
+        result.append(list(map(lambda x: x[i], positions)))
+    # igualations
+    positions = result
+    #print(positions)
+    color_pos = [(80, 120), (80, 170), (80, 220), (80, 270)]
     # game loop
     run = True
+    counter = 0
     while run:
         for event in py.event.get():
+            # insert loop here for all layers
             if event.type == py.MOUSEBUTTONUP:
-                drawCircle()
+                # print circles
+                mouse = py.mouse.get_pos()
+                if clickchecker(color_pos, mouse):
+                    changeColor()
+                    pos=getPos()
+                    changeColor()
+
+                elif clickchecker(positions[counter], mouse):
+                    #returns the position where to start to draw
+                    tupla = clicklayer(positions[counter], mouse)
+                    if color is not None:
+                        switchcolor(tupla)
+                elif clickchecker([(int(0.55 * 800), int(0.90 * 600))], mouse):
+                    # check if the click it's in save
+                    counter += 1
+                #drawCircle()
                 py.display.update()
             if event.type == py.QUIT:
                 run = False
@@ -57,6 +90,7 @@ def inradius(point, radius, mouse):
 
 def changeColor():
     global color
+    # catch the click
     mouse = py.mouse.get_pos()
     if inradius((80, 120), 20, mouse):
         color = RED
@@ -71,11 +105,28 @@ def getPos():
     pos = py.mouse.get_pos()
     return pos
 
-def drawCircle():
+def drawCircle(r):
     changeColor()
     pos=getPos()
     changeColor()
     py.draw.circle(screen, color, pos, 20)
+
+def clickchecker(layer_fun, click):
+    for i in layer_fun:
+        if inradius(i, 20, click):
+            return True
+    return False
+
+def clicklayer(layer_fun, click):
+    changeColor()
+    pos=getPos()
+    changeColor()
+    for i in layer_fun:
+        if inradius(i, 20, click):
+            return i
+
+def switchcolor(tupla):
+    py.draw.circle(screen, color, tupla, 20)
 
 if __name__ == '__main__':
     main()
